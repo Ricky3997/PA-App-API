a17297d7ad10a57b084306cfa7a54a50049f8934
Ricky3997 <riccardo@broggi.co.uk>
2018-12-01 17:53:49 +0000

    Manual matching vs automated matching

--- UI/src/admin/matching/Matching.js
+++ UI/src/admin/matching/Matching.js
@@ -1,31 +1,43 @@
 import React, {Component} from 'react';
-import {Col, Container, Image, ListGroup, Row, CardDeck} from "react-bootstrap";
+import {Col, Container, Image, ListGroup, Row, CardDeck, Button, InputGroup, CardColumns, Form} from "react-bootstrap";
 import MentorCard from "../mentors/MentorCard";
 import * as _ from 'lodash';
+import {Icon} from "react-fa";
+import * as JsSearch from "js-search";
 
 class Matching extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            active: null
+            active: null,
+            manualMode: false,
+            search: ""
         };
+        this.search = new JsSearch.Search('id');
+        this.search.addIndex('firstName');
+        this.search.addIndex('lastName');
+        this.search.addIndex('university');
+        this.search.addIndex('course');
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
         this.setState({active: nextProps.mentees.length > 0 ? nextProps.mentees[0].id : null})
+        this.search.addDocuments(nextProps.mentors);
     }
 
     render() {
-        const rand = _.random(0,this.props.mentors.length);
+        const rand = _.random(0, this.props.mentors.length - 3);
         const toMatch = this.state.active ? this.props.mentees.filter(m => m.id === this.state.active)[0] : null;
+        const mentorsToRender = this.state.search.length > 0 ? this.search.search(this.state.search) : this.props.mentors;
         return (
             <Row>
                 <Col md={3}>
                     <ListGroup>
-                        {this.props.mentees.length === 0 ? <ListGroup.Item active>No Mentees To Match</ListGroup.Item> : null}
+                        {this.props.mentees.length === 0 ?
+                            <ListGroup.Item active>No Mentees To Match</ListGroup.Item> : null}
                         {this.props.mentees.map(m => <ListGroup.Item active={this.state.active === m.id}
-                                                                onClick={() => this.setState({active: m.id})}
-                                                                style={{cursor: "pointer"}}>
+                                                                     onClick={() => this.setState({active: m.id})}
+                                                                     style={{cursor: "pointer"}}>
                                 <Image roundedCircle alt="Mentee avatar" src={m.pictureUrl} style={{width: "30px"}}/>
                                 {`  ${m.firstName}`}
                             </ListGroup.Item>
@@ -37,7 +49,8 @@ class Matching extends Component {
                         <Container fluid>
                             <Row>
                                 <Col md={2}>
-                                    <Image rounded alt="Mentor avatar" src={toMatch.pictureUrl} style={{width: "150px"}}/>
+                                    <Image rounded alt="Mentor avatar" src={toMatch.pictureUrl}
+                                           style={{width: "150px"}}/>
                                 </Col>
                                 <Col md={10}>
                                     <h4>{`${toMatch.firstName}`}</h4>
@@ -46,13 +59,47 @@ class Matching extends Component {
                                     <h6>{`Subjects: ${toMatch.subjectsInSchool.toString()}`}</h6>
                                 </Col>
                             </Row>
+                            <br/>
+                            <Row>
+                                <Col md={4}>
+                                    <h5>{this.state.manualMode ? "Manual Matching" : "Top 3 Recommended Matches"}</h5>
+                                </Col>
+                                <Col md={8}>
+                                    <Button variant="secondary"
+                                            onClick={() => this.setState({manualMode: !this.state.manualMode})}>
+                                        {this.state.manualMode ?
+                                            <span>Or See Automated Reccomendations <Icon name="fas fa-magic"/></span>
+                                            :
+                                            <span> Or Manually Search for a Mentor <Icon name="fas fa-search"/></span>}
+                                    </Button>
+                                </Col>
+                            </Row>
                             <br />
                             <Row>
-                                <Col md={8}>
-                                    <h5>Suggested Mentors:</h5>
-                                    <CardDeck>
-                                        {this.props.mentors.slice(rand, rand + 2).map(m => <MentorCard key={m.id} {...m} matching />)}
-                                    </CardDeck>
+                                <Col>
+                                    {this.state.manualMode ? <div>
+                                        <Form.Group>
+                                            <InputGroup className="mb-3">
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Text><Icon name="fas fa-search"/></InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                                <Form.Control placeholder="Search..." value={this.state.search}
+                                                              onChange={e => this.setState({search: e.target.value})}/>
+                                            </InputGroup>
+                                        </Form.Group>
+
+                                        <CardColumns>
+                                            {mentorsToRender.map(m => <MentorCard {...m} key={m.id} matching/>)}
+                                        </CardColumns>
+
+                                    </div> :
+                                        <CardDeck>
+                                            {this.props.mentors.slice(rand, rand + 3).map(m => <MentorCard
+                                                key={m.id} {...m} matching/>)}
+                                        </CardDeck>
+                                    }
+                                    <br/>
+
                                 </Col>
                             </Row>
                         </Container> : <Container fluid>
