1da29a5722c2ef5773c02e75ef0357f913a47432
Ricky3997 <riccardo@broggi.co.uk>
2019-02-19 15:25:40 +0000

    Version change

--- UI/src/components/various/Footer.js
+++ UI/src/components/various/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
       </Badge>
     </Navbar.Brand>
     <Navbar.Text>
-      This is a work in progress, bear with us! V0.1.100219
+      This is a work in progress, bear with us! V0.1.2019-02-19
     </Navbar.Text>
     <Navbar.Collapse className="justify-content-end">
       <Navbar.Text>
