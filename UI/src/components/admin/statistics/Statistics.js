cff9fd043f17885a4d2a50fac986e6d99eec7a44
Ricky3997 <riccardo@broggi.co.uk>
2019-03-31 21:04:22 +0200

    gender in data tab

--- UI/src/components/admin/statistics/Statistics.js
+++ UI/src/components/admin/statistics/Statistics.js
@@ -1,20 +1,40 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import DataVisHeatMap from "./DataVisHeatMap";
 import Moment from "moment";
 import SignupsChart from "./SIgnupsChart";
+import ButtonNotReadyYet from "../../various/ButtonNotReadyYet";
+import { Icon } from "react-fa";
+import {Doughnut} from 'react-chartjs'
+import GenderDoughnut from "./GenderDoughnut";
 
 
 const Statistics = ({ mentors, mentees }) => {
 
-
   return (
     <Container fluid>
+      <Row>
+        <Col md={{offset: 9}}>
+          <ButtonNotReadyYet>
+            <Button disabled block>
+              <Icon name="fas fa-file-text" /> Export to PDF
+            </Button>
+          </ButtonNotReadyYet>
+
+        </Col>
+      </Row>
       <Row>
         <Col>
           <h3>
             Mentors
           </h3>
+          <div>
+            <span  style={{ fontWeight: "bold", fontSize: "30px" }}>
+              {mentors.length}
+            </span>
+            <span>
+              in total,
+            </span>
           <span style={{ fontWeight: "bold", fontSize: "30px" }}>
             <span style={{ color: "#289b00" }}>
               +{mentors.filter(m => Moment.duration(new Moment().diff(new Moment(m.latestStatusChange))).asDays() <= 7).length}
@@ -23,11 +43,24 @@ const Statistics = ({ mentors, mentees }) => {
           <span>
             {" mentors in the last week"}
           </span>
+          </div>
+          <h5>
+            Gender
+          </h5>
+          <GenderDoughnut users={mentors}/>
         </Col>
         <Col>
           <h3>
             Mentees
           </h3>
+
+          <div>
+            <span  style={{ fontWeight: "bold", fontSize: "30px" }}>
+              {mentees.length}
+            </span>
+            <span>
+              in total,
+            </span>
           <span style={{ fontWeight: "bold", fontSize: "30px" }}>
             <span style={{ color: "#289b00" }}>
               +{mentees.filter(m => Moment.duration(new Moment().diff(new Moment(m.latestStatusChange))).asDays() <= 7).length}
@@ -36,6 +69,11 @@ const Statistics = ({ mentors, mentees }) => {
           <span>
             {" mentees in the last week"}
           </span>
+          </div>
+          <h5>
+            Gender
+          </h5>
+          <GenderDoughnut users={mentees}/>
         </Col>
       </Row>
       <Row className="justify-content-md-center">
@@ -44,7 +82,7 @@ const Statistics = ({ mentors, mentees }) => {
           <SignupsChart mentees={mentees} mentors={mentors}/>
         </Col>
       </Row>
-      <h3>Rastio of mentees/mentors</h3>
+      <h3>Ratio of mentees/mentors</h3>
       <Row style={{marginTop: '-100px', marginLeft: '-40px'}}>
         <Col>
         <DataVisHeatMap mentors={mentors} mentees={mentees}/>
