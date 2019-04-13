31b0e949945006b59c07d12df0deed760b69f261
Ricky3997 <riccardo@broggi.co.uk>
2019-04-13 21:03:55 +0100

    Fixed signups chart context issue

--- UI/src/components/admin/statistics/SignupTrendsChart.js
+++ UI/src/components/admin/statistics/SignupTrendsChart.js
@@ -6,21 +6,21 @@ const SignupTrendsChart = ({ mentors, mentees, from, to }) => {
 
   const range = Math.ceil(Moment.duration(Moment(to).diff(Moment(from))).asDays()) + 1;
   let labels, menteeData, mentorData;
-  // if (range <= 7) {
+  if (range <= 7) {
   labels = [...Array(range).keys()].reverse().map(d => new Moment(to).subtract(d, "d").format("MMM DD"));
   menteeData = [...Array(range).keys()].reverse().map(d => mentees.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, "d"))).length);
   mentorData = [...Array(range).keys()].reverse().map(d => mentors.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, "d"))).length);
-  // } else if (range <= 35) {
-  //   labels = [...Array(Math.ceil(range / 7)).keys()].reverse().map(d => new Moment(to).subtract(d, "w").format("MMM DD")).map(d => `Week of ${d}`);
-  //   menteeData = [...Array(Math.ceil(range / 7)).keys()].reverse().map(d => mentees.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, 'w'))).length);
-  //   mentorData = [...Array(Math.ceil(range / 7)).keys()].reverse().map(d => mentors.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, 'w'))).length);
-  // } else {
-  //   labels = [...Array(Math.ceil(range / 30)).keys()].reverse().map(d => new Moment(to).subtract(d, "M").format("MMM"));
-  //   menteeData = [...Array(Math.ceil(range / 30)).keys()].reverse().map(d => mentees.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, 'M'))).length);
-  //   mentorData = [...Array(Math.ceil(range / 30)).keys()].reverse().map(d => mentors.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, 'M'))).length);
-  // } TODO Need to fix issue that react vis uses chartjs under the covers, and additive behaviour for new data means changign format of axes fucks up th datasets
+  } else if (range <= 35) {
+    labels = [...Array(Math.ceil(range / 7)).keys()].reverse().map(d => new Moment(to).subtract(d, "w").format("MMM DD")).map(d => `Week of ${d}`);
+    menteeData = [...Array(Math.ceil(range / 7)).keys()].reverse().map(d => mentees.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, 'w'))).length);
+    mentorData = [...Array(Math.ceil(range / 7)).keys()].reverse().map(d => mentors.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, 'w'))).length);
+  } else {
+    labels = [...Array(Math.ceil(range / 30)).keys()].reverse().map(d => new Moment(to).subtract(d, "M").format("MMM"));
+    menteeData = [...Array(Math.ceil(range / 30)).keys()].reverse().map(d => mentees.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, 'M'))).length);
+    mentorData = [...Array(Math.ceil(range / 30)).keys()].reverse().map(d => mentors.filter(m => new Moment(m.latestStatusChange).isBetween(1262304000, new Moment(to).subtract(d, 'M'))).length);
+  } //TODO Need to fix issue that react vis uses chartjs under the covers, and additive behaviour for new data means changign format of axes fucks up th datasets
 
-  return <Line
+  return <Line redraw
     data={{
       labels: labels,
       datasets: [
