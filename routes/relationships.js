ac72b5b2ae068477903359b12e1da275aa3b7f60
Ricky3997 <riccardo@broggi.co.uk>
2019-05-09 22:49:33 +0100

    Finished refactoring, most likely broke some stuff but when bugs found and fixed this new structure should be more resilient

--- routes/relationships.js
+++ routes/relationships.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const relationshipsService = require('../service/relationships');
+const { Relationship } = require('./../models/relationship');
+const { Mentee } = require('./../models/mentees');
 
 router.get('/', async (req, res) => {
-  const result = await relationshipsService.getAll();
-  if (result) res.json(result);
-  else res.sendStatus(400);
+  const rels = await Relationship.find()
+    .populate({ path: 'mentee', populate: { path: 'relationship' } })
+    .populate({ path: 'mentor', populate: { path: 'relationship' } }).exec();
+  res.json(rels);
 });
 
 router.post('/mentorDecision/:relationshipId', async (req, res) => {
   const { id } = req.decoded;
   const { relationshipId } = req.params;
   const { accept } = req.body;
-  const result = await relationshipsService.mentorDecision(relationshipId, id, accept);
-  if (result) res.json(result);
-  else res.sendStatus(400);
+
+  const rel = await Relationship.findById(relationshipId);
+  if (rel.mentor.toString() !== id) res.status(401).json({error: 'Mentor in relationship is not active user'});
+  if (accept) {
+    await Relationship.findById(relationshipId).update({ status: 'confirmed' }).exec();
+    const rel =  await Relationship.findById(relationshipId).populate({ path: 'mentee', populate: { path: 'relationship' } })
+      .populate({ path: 'mentor', populate: { path: 'relationship' } }).exec();
+    res.json(rel);
+    //TODO Send email notification
+  } else {
+    await relationshipsService.deleteRelationship(relationshipId, id, rel.mentee);
+    await Mentee.findByIdAndUpdate(rel.mentee, { $push: { mentorBlackList: id } }).exec();
+    res.json(true)
+  }
 });
 
 module.exports = router;
--- service/relationships.js
+++ service/relationships.js
@@ -5,20 +5,10 @@ const { Mentee } = require('./../models/mentees');
 const _ = require('lodash');
 const moment = require('moment');
 
-getAll = async () => {
-  return await Relationship.find().populate({ path: 'mentee', populate: { path: 'relationship' } })
-    .populate({ path: 'mentor', populate: { path: 'relationship' } }).exec().then(p => {
-      return p;
-    });
-};
-
 deleteRelationship = async (relationshipId, mentorId, menteeId) => {
-  await Mentor.findByIdAndUpdate(mentorId, { $pull: { relationship: relationshipId } }).exec().then(p => {
-    return p;
-  });
+  await Mentor.findByIdAndUpdate(mentorId, { $pull: { relationship: relationshipId } }).exec();
   await Mentee.findByIdAndUpdate(menteeId,{ relationship: null }).exec();
   await Relationship.deleteOne({ _id: relationshipId });
-  console.log(`Deleted {${relationshipId}}`);
 };
 
 checkForElapsedMatches = async () => {
@@ -27,23 +17,5 @@ checkForElapsedMatches = async () => {
   await rels.forEach(async r => await deleteRelationship(r._id, r.mentor, r.mentee));
 };
 
-mentorDecision = async (relationshipId, mentorId, accept) => {
-  const rel = await Relationship.findById(relationshipId);
-  if (rel.mentor.toString() !== mentorId) return null;
-  if (accept) {
-    await Relationship.findById(relationshipId).update({ status: 'confirmed' }).exec();
-    return await Relationship.findById(relationshipId).populate({ path: 'mentee', populate: { path: 'relationship' } })
-      .populate({ path: 'mentor', populate: { path: 'relationship' } }).exec().then(p => {
-        return p;
-      });
-    //TODO Send email notification
-  } else {
-    await deleteRelationship(relationshipId, mentorId, rel.mentee);
-    await Mentee.findByIdAndUpdate(rel.mentee, { $push: { mentorBlackList: mentorId } }).exec();
-    return true;
-    //TODO ADD TO BLACKLIST
-    //TODO Hourly check for this
-  }
-};
 
-module.exports = { getAll, mentorDecision, checkForElapsedMatches, deleteRelationship };
+module.exports = { checkForElapsedMatches, deleteRelationship };
